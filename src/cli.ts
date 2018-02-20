29dd6947bddcf9039e77dda14aaf29b890e88d27
Zoltan Kochan <z@kochan.io>
2018-02-20 23:13:18 +0200

    feat: add help command

--- src/cli.ts
+++ src/cli.ts
@@ -3,11 +3,33 @@ import publishPacked from '.'
 import getopts = require('getopts')
 
 const opts = getopts(process.argv.slice(2), {
-  boolean: ['prune'],
+  alias: {
+    h: 'help',
+  },
+  boolean: [
+    'help',
+    'prune',
+  ],
   default: {
+    help: false,
     prune: false,
     tag: 'latest',
   },
 })
 
-publishPacked(process.cwd(), opts)
+if (opts._[0] === 'help' || opts.help) {
+  console.log(`
+  Usage: publish-packed [flags]
+
+  Options:
+
+    -h, --help  Output usage information
+
+    --tag       Registers the published package with the given tag, such that npm install <name>@<tag> will install this version.
+                By default, npm publish updates and npm install installs the latest tag.
+
+    --prune     Prune unneeded files (.md, .td, etc..) from node_modules folder.
+  `)
+} else {
+  publishPacked(process.cwd(), opts)
+}
